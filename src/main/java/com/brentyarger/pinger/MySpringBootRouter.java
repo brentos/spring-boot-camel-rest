12bbc29ccab8a0bd86b0df963a5c61f9ad0cb338
Brent Yarger <brent.yarger@yahoo.com>
2015-06-24 09:04:07 -0700

    Removed ruby, added spring

--- src/main/java/com/brentyarger/pinger/MySpringBootRouter.java
+++ src/main/java/com/brentyarger/pinger/MySpringBootRouter.java
@@ -1,11 +1,19 @@
 package com.brentyarger.pinger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.apache.camel.component.restlet.RestletComponent;
 import org.apache.camel.component.restlet.RestletConstants;
 import org.apache.camel.spring.boot.FatJarRouter;
+import org.restlet.Component;
 import org.restlet.Request;
+import org.restlet.ext.spring.SpringServerServlet;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySpringBootRouter extends FatJarRouter {
@@ -13,7 +21,7 @@ public class MySpringBootRouter extends FatJarRouter {
     @Override
     public void configure() {
     	
-    	restConfiguration().component("restlet").host("localhost").port(9000);
+    	restConfiguration().component("restlet");
     	
 //        from("timer://trigger").
 //                transform().simple("ref:myBean").
@@ -33,7 +41,46 @@ public class MySpringBootRouter extends FatJarRouter {
         
         
     }
-
+//
+//    @Bean
+//    public ServletContextInitializer initializer() {
+//    	return new ServletContextInitializer() {
+//
+//			public void onStartup(ServletContext servletContext)
+//					throws ServletException {
+//				servletContext.setInitParameter("org.restlet.component", "RestletComponent");
+//			}
+//    		
+//    	};
+//    }
+    
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+    	
+    	SpringServerServlet serverServlet = new SpringServerServlet();
+    	ServletRegistrationBean regBean = new ServletRegistrationBean( serverServlet, "/rest/*");
+    	
+    	
+    	Map<String,String> params = new HashMap<String,String>();
+    	
+    	params.put("org.restlet.component", "restletComponent");
+    	
+    	regBean.setInitParameters(params);
+    	
+    	return regBean;
+    }
+    
+    
+    @Bean
+    public Component restletComponent() {
+    	return new Component();
+    }
+    
+    @Bean
+    public RestletComponent restletComponentService() {
+    	return new RestletComponent(restletComponent());
+    }
+    
 //    @Bean
 //    String myBean() {
 //        return "I'm Spring bean!";
